2330f601b3b7cf6488173741286cfc11cdee5ba8
Paul Bailey <paul@neutron.studio>
2021-08-18 09:25:44 -0500

    download url

--- nsync_server/nstore/schema.py
+++ nsync_server/nstore/schema.py
@@ -62,7 +62,7 @@ class FileVersionNode(DjangoObjectType):
 	class Meta:
 		model = FileVersion
 		filter_fields = ['id', 'sync_file']
-		fields = ('created', 'id', 'transaction', 'uhash', 'permissions')
+		fields = ('created', 'id', 'transaction', 'uhash', 'permissions', 'is_dir')
 		interfaces = (relay.Node, )
 
 	@classmethod
