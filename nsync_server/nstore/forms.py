b077ac6fefe42d45e7e920a6391f6ab132d52baf
Paul Bailey <paul@neutron.studio>
2021-08-18 08:16:27 -0500

    form tweaks

--- nsync_server/nstore/forms.py
+++ nsync_server/nstore/forms.py
@@ -41,6 +41,7 @@ class SaveVersionForm(forms.Form):
 		version.save()
 
 		if self.cleaned_data['ebody']:
+			print(type(self.cleaned_data['ebody']), self.cleaned_data['ebody'])
 			content = ContentFile(self.cleaned_data['ebody'])
 			filename = '{}.etxt'.format(uuid.uuid4())
 			version.efile.save(filename, content=content)
