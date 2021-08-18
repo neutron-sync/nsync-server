ecaefec737432be5373bd30ad8d29c88800c7cc0
Paul Bailey <paul@neutron.studio>
2021-08-17 20:40:14 -0500

    django storages

--- nsync_server/nsync/settings/prod.py
+++ nsync_server/nsync/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from nsync_server.nsync.settings.base import *
 
 DEBUG = False
@@ -5,3 +7,12 @@ DEBUG = False
 STATIC_ROOT = BASE_DIR / 'static-compiled'
 
 SECURE_SSL_REDIRECT = True
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY= os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME= os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = 'private'
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
