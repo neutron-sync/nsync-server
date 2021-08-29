98b9a8e6cb323762a702a382c23ac80ab71ecbd9
Paul Bailey <paul@neutron.studio>
2021-08-29 16:25:31 -0500

    key exchange

--- nsync_server/nsync/graphene.py
+++ nsync_server/nsync/graphene.py
@@ -4,7 +4,6 @@ from graphql import GraphQLError
 class AuthMutation:
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
-        print('NARF')
         if info.context.user.is_authenticated:
             if info.context.user.username != 'tokenuser':
                 return super().mutate_and_get_payload(root, info, **input)
