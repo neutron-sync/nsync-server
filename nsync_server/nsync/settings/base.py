9b41ade7f943a2f19f367fb63be28880c2121380
Paul Bailey <paul@neutron.studio>
2021-09-06 15:21:20 -0500

    update cookie settings

--- nsync_server/nsync/settings/base.py
+++ nsync_server/nsync/settings/base.py
@@ -119,9 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-SESSION_COOKIE_SAMESITE = 'Strict'
-CSRF_COOKIE_SAMESITE = 'Strict'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
