0ab5505750254ab8a4d65e50501b0a5a13820a60
Paul Bailey <paul@neutron.studio>
2021-09-20 20:24:39 -0500

    dummy cache

--- nsync_server/nsync/settings/dev.py
+++ nsync_server/nsync/settings/dev.py
@@ -3,3 +3,9 @@ from nsync_server.nsync.settings.base import *
 DEBUG = True
 
 MEDIA_ROOT = BASE_DIR / '..' / 'uploads'
+
+CACHES = {
+  'default': {
+		'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+  }
+}
