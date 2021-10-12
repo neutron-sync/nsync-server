c8f52ba2b84efce266e15cf3d6d63c8e871890cf
Paul Bailey <paul@neutron.studio>
2021-10-12 09:06:26 -0500

    transaction filter

--- nsync_server/nstore/schema.py
+++ nsync_server/nstore/schema.py
@@ -96,7 +96,7 @@ class FileVersionNode(DjangoObjectType):
 
   class Meta:
     model = FileVersion
-    filter_fields = ['id', 'sync_file', 'transaction']
+    filter_fields = ['id', 'sync_file', 'transaction__lte']
     fields = ('created', 'id', 'transaction', 'uhash', 'permissions', 'timestamp', 'is_dir', 'sync_file', 'transaction')
     interfaces = (relay.Node,)
 
