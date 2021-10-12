995e70add7747306f775c008e4976564e0d52223
Paul Bailey <paul@neutron.studio>
2021-10-12 07:45:14 -0500

    add uhash index

--- nsync_server/nstore/schema.py
+++ nsync_server/nstore/schema.py
@@ -96,7 +96,7 @@ class FileVersionNode(DjangoObjectType):
 
   class Meta:
     model = FileVersion
-    filter_fields = ['id', 'sync_file']
+    filter_fields = ['id', 'sync_file', 'uhash']
     fields = ('created', 'id', 'transaction', 'uhash', 'permissions', 'timestamp', 'is_dir', 'sync_file', 'transaction')
     interfaces = (relay.Node,)
 
